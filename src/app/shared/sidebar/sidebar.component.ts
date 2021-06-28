ee9ef3cd38c4d47c254c91524f0ed7ebdbb66758
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-06-27 21:56:28 -0500

    feat(GifsService)

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,15 @@
 import { Component } from "@angular/core";
 
+import { GifsService } from "src/app/gifs/services/gifs.service";
+
 @Component({
     selector: 'app-sidebar',
     templateUrl: 'sidebar.component.html'
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+    get historial() {
+        return this.gifsService.historial;
+    }
+
+    constructor(private gifsService: GifsService) {}
+}
