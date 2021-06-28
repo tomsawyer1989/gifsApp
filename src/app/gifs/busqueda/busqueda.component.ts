05f49d6139f53a00d8c06ff0d48dbbd637552f83
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-06-27 20:40:41 -0500

    feat(@ViewChild() - Obtener referencias a objetos del HTML)

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-busqueda',
     templateUrl: 'busqueda.component.html'
 })
-export class BusquedaComponent {}
+export class BusquedaComponent {
+    @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+    
+    buscar () {
+        const valor = this.txtBuscar.nativeElement.value;
+
+        console.log(valor);
+
+        this.txtBuscar.nativeElement.value = '';
+    }
+}
